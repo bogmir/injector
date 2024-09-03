9f2ff2a2ff00ce3acfa9388c953309b91298645f
bogdan <bogdan.mircea@coosto.com>
2024-09-03 19:22:05 +0200

    add alias of module before compile time

--- lib/injector.ex
+++ lib/injector.ex
@@ -1,25 +1,32 @@
 defmodule Injector do
-  defmacro inject(module) when is_atom(module) do
-    raise CompileError, description: "option :as is required for injecting erlang modules"
+  defmacro __using__(_opts) do
+    quote do
+      import Injector
+      @before_compile Injector
+    end
   end
 
-  defmacro inject(module = {:__aliases__, meta, aliases}) do
-    inject_as(module, as: {:__aliases__, meta, [List.last(aliases)]})
-  end
-
-  defmacro inject(module, as) do
-    inject_as(module, as)
-  end
-
-  defp inject_as({:__aliases__, _, aliases}, as) do
-    aliases |> Module.concat() |> inject_as(as)
-  end
-
-  defp inject_as(module, as: as) do
+  defmacro inject(module, opts \\ []) do
+    as = Keyword.get(opts, :as, module)
     injection = Application.get_env(:injector, module, module)
 
     quote do
-      alias unquote(injection), as: unquote(as)
+      @injections {unquote(injection), unquote(as)}
+    end
+  end
+
+  defmacro __before_compile__(env) do
+    injections = Module.get_attribute(env.module, :injections) || []
+
+    aliases =
+      for {injection, as} <- injections do
+        quote do
+          alias unquote(injection), as: unquote(as)
+        end
+      end
+
+    quote do
+      (unquote_splicing(aliases))
     end
   end
 end
